d2adab2760848dfb7999f690501eca41b5a3d97e
Harsh-Srivastava <1by23ai058@bmsit.in>
2026-03-13 04:01:28 +0530

    vercel fix

--- src/Components/Linkspanel.jsx
+++ src/Components/Linkspanel.jsx
@@ -1,4 +1,5 @@
 import "../css/links.css";
+import { Link } from "react-router-dom";
 
 const socialLinks = [
   {
@@ -79,7 +80,7 @@ export default function LinksPanel() {
         </div>
       </div>
 
-      <a href="/projects" className="links-panel__project">
+      <Link to="/projects" className="links-panel__project">
         <div className="links-panel__project-icon">
           <svg viewBox="0 0 24 24" width="32" height="32" fill="none" stroke="currentColor" strokeWidth="1.5">
             <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
@@ -91,7 +92,7 @@ export default function LinksPanel() {
           <span>工</span>
           <span>芸</span>
         </div>
-      </a>
+      </Link>
     </div>
   );
 }
